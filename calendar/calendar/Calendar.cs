5df07ccc45bc08a5b509759bf2c06e21e51cecba
Suhoy95 <suhoy95@gmail.com>
2014-11-24 03:58:48 +0400

    Класс по созданию матрицы месяца

---
+++ calendar/calendar/Calendar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calendar
+{
+    class Calendar
+    {
+        /**
+         * Класс описывающий сетку месяца
+         * date - выделенный день
+         * mouthMap - карта месяца, каждое число - день месяца
+         */
+        public DateTime date;
+        public int[][] mouthMap;
+
+        public Calendar(DateTime newdate)
+        {
+            date = newdate;
+            mouthMap = new int[6][].Select(x => new int[7]).ToArray();
+            
+            DateTime curDate = newdate.AddDays(-newdate.Day);
+            while ((int) curDate.DayOfWeek != 1)
+                curDate = curDate.AddDays(-1);
+
+            for (int week = 0; week < 6; week++)
+            {
+                for (int day = 0; day < 7; day++)
+                {
+                    mouthMap[week][day] = curDate.Month == date.Month ? curDate.Day : - curDate.Day;
+                    curDate = curDate.AddDays(1);
+                }
+            }
+        }
+
+    }
+}
---
+++ calendar/calendar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calendar x = new Calendar(new DateTime(2011, 11, 16));
+            for (int week = 0; week < 6; week++)
+            {
+                for(int day = 0; day < 7; day++)
+                    Console.Write(string.Format("{0,4}", x.mouthMap[week][day]));
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
