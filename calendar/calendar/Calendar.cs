f785e787882cc068e978a198d2e1c3e860a337eb
Suhoy95 <suhoy95@gmail.com>
2014-11-27 00:28:34 +0400

    Refactoring

--- calendar/calendar/Calendar.cs
+++ calendar/calendar/Calendar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,76 @@ namespace calendar
 
             return mouthMap;
         }
+    }
 
+    class Calendar_renderer
+    {
+        //possition
+        private static int width, height;
+        private static int fontSize;
+        //draw
+        private static Font drawFont;
+        private static SolidBrush drawBrush = new SolidBrush(System.Drawing.Color.Black);
+        private static StringFormat drawFormat = new StringFormat();
+
+
+        public static void Render(Graphics source, DateTime date, int newWidth, int newHeight)
+        {
+            InitParams(newWidth, newHeight);
+            DrawBackground(source);
+            DrawMouthInfo(source, date);
+            DrawMouthMap(source, date);
+        }
+        
+        private static void InitParams(int newWidth, int newHeight)
+        {
+            width = newWidth;
+            height = newHeight;
+            fontSize = width/14;
+            drawFont = new Font("Arial", fontSize);
+            drawFormat.Alignment = StringAlignment.Center;
+            drawFormat.LineAlignment = StringAlignment.Center;
+        }
+
+        private static void DrawBackground(Graphics source)
+        {
+            SolidBrush brush = new SolidBrush(Color.White);
+            source.FillRectangle(brush, new Rectangle(0, 0, width, height));
+            brush.Dispose();
+        }
+
+        private static void DrawMouthMap(Graphics source, DateTime date)
+        {
+            var mouthMap = Calendar.GetMothMap(date);
+            for (int week = 0; week < 6; week++)
+                for (int day = 0; day < 7; day++)
+                    DrawDay(source, mouthMap[week][day], week, day);
+        }
+
+        private static void DrawMouthInfo(Graphics source, DateTime date)
+        {
+            string mouthName = date.ToString("M");
+            Point possition = new Point(width / 2, height / 16);
+            source.DrawString(mouthName, drawFont, drawBrush, possition.X, possition.Y, drawFormat);
+
+            for(int i =0; i < 7; i++)
+                DrawDayName(source, i);
+        }
+
+        private static int DrawDayName(Graphics canvas, int i)
+        {
+            string[] days = { "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс" };
+            string day = days[i];
+            Point possition = new Point(width / 7 * i + width/14, height/8 + height/16);
+            canvas.DrawString(day, drawFont, drawBrush, possition.X, possition.Y, drawFormat);
+            return 0;
+        }
+
+        private static void DrawDay(Graphics canvas, int p, int week, int day)
+        {
+            SolidBrush dayBrush = new SolidBrush(p < 0 ? Color.Gray : Color.Black);
+            Point possition = new Point(width / 7 * day + width/16, height / 8 * week + height/4 + height/16);
+            canvas.DrawString(Math.Abs(p).ToString(), drawFont, dayBrush, possition.X, possition.Y, drawFormat);
+        }
     }
 }
