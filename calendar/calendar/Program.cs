7f518bd4bbd03d4807dc34f787d63579f980bf13
Suhoy95 <suhoy95@gmail.com>
2014-12-02 21:10:54 +0400

    Возможность задания размеров и печать страниц на весь год

--- calendar/calendar/Program.cs
+++ calendar/calendar/Program.cs
@@ -10,6 +10,7 @@ namespace calendar
 {
     class Program
     {
+        private static int width = 800, height = 600;
      
         public static int Main(string[] args)
         {
@@ -18,16 +19,68 @@ namespace calendar
                 Console.WriteLine("Пожалуйста введите дату для формаирования календаря");
                 return -1;
             }
-            DateTime date = new DateTime();
-            if (DateTime.TryParse(args[0], out date))
+            for(var i = 0; i < args.Length; i++)
             {
-                var img_data = Calendar_data_builder.GetMothMap(date);
-                var img = Calendar_renderer.Render(img_data, 80 * 7, 60 * 8);
-                img.Save(string.Format("{0}_{1}_{2}.png", date.Day, date.Month, date.Year));
+                if(args[i][0] == '-')
+                {
+                    switch(GetParams(args[i]))
+                    {
+                        case 's':
+                            if(++i >= args.Length)
+                            {
+                                Console.WriteLine("Введите размеры (000X000)");
+                                return -1;
+                            }
+                            var size = args[i].Split(new char[] { 'x', 'X' }).Select(x => int.Parse(x)).ToArray();
+                            width = size[0];
+                            height = size[1];
+                            break;
+                        case 'y':
+                            if (++i >= args.Length)
+                            {
+                                Console.WriteLine("Введите год календаря");
+                                return -1;
+                            }
+                            int year;
+                            if(int.TryParse(args[i], out year))
+                            {
+                                for (var j = 1; j <= 12; j++)
+                                    CreateCalendarList(new DateTime(year, j, 1), false);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Не удалось преобразовать год календаря");
+                            }
+                            break;
+                    }
+                } 
+                else
+                {
+                    DateTime date = new DateTime();
+                    if (DateTime.TryParse(args[i], out date))
+                        CreateCalendarList(date);
+                    else
+                    {
+                        Console.WriteLine("Не удалось преобразовать дату");
+                        return -1;
+                    }
+                    break;
+                }
             }
-            else
-                Console.WriteLine("Не удалось преобразовать дату");
+                     
             return 0;
+        }
+
+        private static char GetParams(string p)
+        {
+            return p.Length < 2 ? ' ' : p[1];
+        }
+
+        private static void CreateCalendarList(DateTime date, bool selected = true)
+        {
+            var img_data = Calendar_data_builder.GetMothMap(date, selected);
+            var img = Calendar_renderer.Render(img_data, width, height);
+            img.Save(string.Format("{0}_{1}_{2}.png", date.Day, date.Month, date.Year));
         }
     }
 }
